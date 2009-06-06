20593fc012e6aab4f95f5b7c15990c9df3c2adc3
Michael Marineau <mmarineau@itasoftware.com>
2009-06-06 21:01:16 +0000

    Replace the xmlrpc outage commands with more powerful ones using parsed expressions

--- python/nagcat/nagios_api.py
+++ python/nagcat/nagios_api.py
@@ -15,6 +15,7 @@
 """Nagios command writer"""
 
 import os
+import re
 import time
 import random
 
@@ -92,6 +93,8 @@ class NagiosCommander(object):
 
 class NagiosXMLRPC(xmlrpc.XMLRPC):
     """A XMLRPC Protocol for Nagios"""
+
+    EXPR_TOKEN = re.compile(r"""\s*([\w:-]+|\(|\)|"|')\s*""")
 
     def __init__(self, nagios_cfg):
         xmlrpc.XMLRPC.__init__(self)
@@ -191,20 +194,59 @@ class NagiosXMLRPC(xmlrpc.XMLRPC):
                     "Unknown servicegroup: %r" % servicegroup_name)
         return self._objects['servicegroup'][servicegroup_name]
 
-    def xmlrpc_scheduleServiceDowntime(self, host_name,
-            service_description, start, stop, user, comment):
-        """schedule a service downtime
-        
-        host_name: a single host or a list of hosts
-        service_description: a single service, list, or empty for all
+    def xmlrpc_scheduleServiceDowntime(self, expr, start, stop, user, comment):
+        """schedule service downtime
+
+        expr: an expression defining the set to operate on
+            operators:
+                or (the union of two sets)
+                and (the intersection of two sets)
+            identifiers:
+                host:hostname
+                service:hostname:servicename
+                hostgroup:groupname
+                servicegroup:groupname
+
+            Quotes (' or ") must be placed around service
+            descriptions when they contain whitespace.
         start: date/time to start (in seconds since epoch!)
         stop: date/time to auto-cancel the downtime
         user: identifier defining who/what sent this request
         comment: arbitrary comment about the downtime
-
+        
         returns a key to use to cancel this downtime early
         """
 
+        return self._scheduleDowntime('service',
+                expr, start, stop, user, comment)
+
+    def xmlrpc_scheduleHostDowntime(self, expr, start, stop, user, comment):
+        """schedule host downtime
+
+        expr: an expression defining the set to operate on
+            operators:
+                or (the union of two sets)
+                and (the intersection of two sets)
+            identifiers:
+                host:hostname
+                service:hostname:servicename
+                hostgroup:groupname
+                servicegroup:groupname
+
+            Quotes (' or ") must be placed around service
+            descriptions when they contain whitespace.
+        start: date/time to start (in seconds since epoch!)
+        stop: date/time to auto-cancel the downtime
+        user: identifier defining who/what sent this request
+        comment: arbitrary comment about the downtime
+        
+        returns a key to use to cancel this downtime early
+        """
+
+        return self._scheduleDowntime('host',
+                expr, start, stop, user, comment)
+
+    def _scheduleDowntime(self, type_, expr, start, stop, user, comment):
         try:
             start = int(start)
             stop = int(stop)
@@ -215,221 +257,128 @@ class NagiosXMLRPC(xmlrpc.XMLRPC):
         key = random.randint(100, 999)
         comment += " key:%d" % key
 
-        if isinstance(host_name, str):
-            host_name = [host_name]
-        if not service_description:
-            service_description = None
-        elif isinstance(service_description, str):
-            service_description = [service_description]
+        assert isinstance(type_, str)
+        tokenizer = self._groupTokenizer(expr+')')
+        group_set = self._groupParser(tokenizer, type_)
 
-        for host in host_name:
-            if host not in self._objects['host']:
-                raise xmlrpc.Fault(1, "Unknown host: %r" % host)
+        if not group_set:
+            raise xmlrpc.Fault(1, "expression evaluated to an empty set")
 
-            for service in service_description:
-                if service not in self._objects['service'][host]:
-                    raise xmlrpc.Fault(1, "Unknown service: %r / %r" %
-                            (host, service))
-
-        for host in host_name:
-            if service_description is None:
-                self._cmd(now, 'SCHEDULE_HOST_SVC_DOWNTIME',
-                        host, start, stop, 1, 0, 0, user, comment)
-            else:
-                for service in service_description:
-                    self._cmd(now, 'SCHEDULE_SVC_DOWNTIME', host,
-                            service, start, stop, 1, 0, 0, user, comment)
-
-        return "%d:%d" % (now, key)
-
-    def xmlrpc_scheduleServiceUnionDowntime(self, groups,
-            start, stop, user, comment):
-        """schedule service downtime for a group
-
-        group: a single group or a list, downtime will be set for the
-            union of the listed service groups.
-        start: date/time to start (in seconds since epoch!)
-        stop: date/time to auto-cancel the downtime
-        user: identifier defining who/what sent this request
-        comment: arbitrary comment about the downtime
-        
-        returns a key to use to cancel this downtime early
-        """
-
-        return self._scheduleServiceGroupDowntime(False,
-                groups, start, stop, user, comment)
-
-    def xmlrpc_scheduleServiceIntersectDowntime(self, groups,
-            start, stop, user, comment):
-        """schedule service downtime for a group
-
-        group: a single group or a list, downtime will be set for the
-            intersection of the listed service groups.
-        start: date/time to start (in seconds since epoch!)
-        stop: date/time to auto-cancel the downtime
-        user: identifier defining who/what sent this request
-        comment: arbitrary comment about the downtime
-        
-        returns a key to use to cancel this downtime early
-        """
-
-        return self._scheduleServiceGroupDowntime(True,
-                groups, start, stop, user, comment)
-
-    def _scheduleServiceGroupDowntime(self, intersect, groups,
-            start, stop, user, comment):
-        try:
-            start = int(start)
-            stop = int(stop)
-        except:
-            raise xmlrpc.Fault(1, "start/stop must be integers")
-
-        now = int(time.time())
-        key = random.randint(100, 999)
-        comment += " key:%d" % key
-
-        if isinstance(groups, str):
-            groups = [groups]
-
-        for group in groups:
-            if group not in self._objects['servicegroup']:
-                raise xmlrpc.Fault(1, "Unknown servicegroup: %r" % group)
-
-
-        if len(groups) == 1:
-            self._cmd(now, 'SCHEDULE_SERVICEGROUP_SVC_DOWNTIME',
-                    groups[0], start, stop, 1, 0, 0, user, comment)
-        else:
-            final_services = set(
-                    self._objects['servicegroup'][groups[0]]['members'])
-            for group in groups[1:]:
-                new_services = set(
-                        self._objects['servicegroup'][group]['members'])
-                if intersect:
-                    final_services.intersection_update(new_services)
-                else:
-                    final_services.update(new_services)
-
-            if not final_services:
-                raise xmlrpc.Fault(1, "Group intersection contains no services")
-
-            for service in final_services:
-                self._cmd(now, 'SCHEDULE_SVC_DOWNTIME', service[0],
-                        service[1], start, stop, 1, 0, 0, user, comment)
-
-        return "%d:%d" % (now, key)
-
-    def xmlrpc_scheduleHostDowntime(self, host_name,
-            start, stop, user, comment):
-        """schedule a host downtime
-        
-        host_name: a single host or a list of hosts
-        start: date/time to start (in seconds since epoch!)
-        stop: date/time to auto-cancel the downtime
-        user: identifier defining who/what sent this request
-        comment: arbitrary comment about the downtime
-
-        returns a key to use to cancel this downtime early
-        """
-
-        try:
-            start = int(start)
-            stop = int(stop)
-        except:
-            raise xmlrpc.Fault(1, "start/stop must be integers")
-
-        now = int(time.time())
-        key = random.randint(100, 999)
-        comment += " key:%d" % key
-
-        if isinstance(host_name, str):
-            host_name = [host_name]
-
-        for host in host_name:
-            if host not in self._objects['host']:
-                raise xmlrpc.Fault(1, "Unknown host: %r" % host)
-
-        for host in host_name:
-            self._cmd(now, 'SCHEDULE_HOST_DOWNTIME',
-                    host, start, stop, 1, 0, 0, user, comment)
-
-        return "%d:%d" % (now, key)
-
-    def xmlrpc_scheduleHostUnionDowntime(self, groups,
-            start, stop, user, comment):
-        """schedule host downtime for a group
-
-        group: a single group or a list, downtime will be set for the
-            union of the listed service groups.
-        start: date/time to start (in seconds since epoch!)
-        stop: date/time to auto-cancel the downtime
-        user: identifier defining who/what sent this request
-        comment: arbitrary comment about the downtime
-        
-        returns a key to use to cancel this downtime early
-        """
-
-        return self._scheduleHostGroupDowntime(False,
-                groups, start, stop, user, comment)
-
-    def xmlrpc_scheduleHostIntersectDowntime(self, groups,
-            start, stop, user, comment):
-        """schedule host downtime for a group
-
-        group: a single group or a list, downtime will be set for the
-            intersection of the listed service groups.
-        start: date/time to start (in seconds since epoch!)
-        stop: date/time to auto-cancel the downtime
-        user: identifier defining who/what sent this request
-        comment: arbitrary comment about the downtime
-        
-        returns a key to use to cancel this downtime early
-        """
-
-        return self._scheduleHostGroupDowntime(True,
-                groups, start, stop, user, comment)
-
-    def _scheduleHostGroupDowntime(self, intersect, groups,
-            start, stop, user, comment):
-        try:
-            start = int(start)
-            stop = int(stop)
-        except:
-            raise xmlrpc.Fault(1, "start/stop must be integers")
-
-        now = int(time.time())
-        key = random.randint(100, 999)
-        comment += " key:%d" % key
-
-        if isinstance(groups, str):
-            groups = [groups]
-
-        for group in groups:
-            if group not in self._objects['hostgroup']:
-                raise xmlrpc.Fault(1, "Unknown hostgroup: %r" % group)
-
-        if len(groups) == 1:
-            self._cmd(now, 'SCHEDULE_HOSTGROUP_HOST_DOWNTIME',
-                    groups[0], start, stop, 1, 0, 0, user, comment)
-        else:
-            final_hosts = set(
-                    self._objects['hostgroup'][groups[0]]['members'])
-            for group in groups[1:]:
-                new_hosts = set(
-                        self._objects['hostgroup'][group]['members'])
-                if intersect:
-                    final_hosts.intersection_update(new_hosts)
-                else:
-                    final_hosts.update(new_hosts)
-
-            if not final_hosts:
-                raise xmlrpc.Fault(1, "Group intersection contains no hosts")
-
-            for host in final_hosts:
+        if type_ == 'host':
+            for host in group_set:
                 self._cmd(now, 'SCHEDULE_HOST_DOWNTIME', host,
+                        start, stop, 1, 0, 0, user, comment)
+        else:
+            for host, service in group_set:
+                self._cmd(now, 'SCHEDULE_SVC_DOWNTIME', host, service,
                         start, stop, 1, 0, 0, user, comment)
 
         return "%d:%d" % (now, key)
+
+    def _groupParser(self, tokenizer, return_type):
+        seq = [None, None, None]
+        index = 0
+        for token in tokenizer:
+            if index == 1:
+                if token.lower() in ('and', 'or'):
+                    seq[1] = token.lower()
+                elif token == ')':
+                    return seq[0]
+                else:
+                    raise xmlrpc.Fault(1, "Unexpected token: %r" % token)
+            elif token == '(':
+                seq[index] = self._groupParser(tokenizer, return_type)
+            else:
+                seq[index] = self._groupGetSet(token, return_type)
+
+            if index == 2:
+                if seq[1] == 'and':
+                    seq[0].intersection_update(seq[2])
+                elif seq[1] == 'or':
+                    seq[0].update(seq[2])
+                index = 1
+            else:
+                index += 1
+
+        raise xmlrpc.Fault(1, "Unexpected end of expression")
+
+    def _groupGetSet(self, identifier, return_type):
+        if ':' not in identifier:
+            raise xmlrpc.Fault(1, "Invalid identifier: %r" % name)
+
+        group_type, group_name = identifier.split(':', 1)
+
+        if group_type == 'host':
+            if group_name not in self._objects['host']:
+                raise xmlrpc.Fault(1, "Unknown host: %r" % group_name)
+
+            if return_type == 'host':
+                group_set = [group_name]
+            else:
+                group_set = []
+                for service in self._objects['service'].get(group_name, []):
+                    group_set.append((group_name, service))
+
+        elif group_type == 'service':
+            if ':' not in group_name:
+                raise xmlrpc.Fault(1, "Invalid service: %r" % group_name)
+
+            host_name, service = group_name.split(':', 1)
+
+            if host_name not in self._objects['service']:
+                raise xmlrpc.Fault(1, "Unknown host: %r" % host_name)
+            if service not in self._objects['service'][host_name]:
+                raise xmlrpc.Fault(1, "Unknown service: %r" % service)
+
+            if return_type == 'host':
+                group_set = [host_name]
+            else:
+                group_set = [(host_name, service)]
+
+        elif group_type == 'hostgroup':
+            if group_name not in self._objects['hostgroup']:
+                raise xmlrpc.Fault(1, "Unknown hostgroup: %r" % group_name)
+
+            hosts = self._objects['hostgroup'][group_name]['members']
+            if return_type == 'host':
+                group_set = hosts
+            else:
+                group_set = []
+                for host_name in hosts:
+                    services = self._objects['service'].get(host_name,[])
+                    group_set.extend([(host_name, x) for x in services])
+
+        elif group_type == 'servicegroup':
+            if group_name not in self._objects['servicegroup']:
+                raise xmlrpc.Fault(1, "Unknown servicegroup: %r" % group_name)
+
+            group_set = self._objects['servicegroup'][group_name]['members']
+            if return_type == 'host':
+                group_set = [x[0] for x in group_set]
+
+        else:
+            raise xmlrpc.Fault(1, "Unknown type: %r" % group_type)
+
+        return set(group_set)
+
+    def _groupTokenizer(self, string):
+        while True:
+            match = self.EXPR_TOKEN.match(string)
+            if match and match.group(1) in ('"', "'"):
+                try:
+                    end = string.index(match.end, match.group(1))
+                except ValueError:
+                    raise xmlrpc.Fault(1, "Unterminated string: %s" % string)
+                token = string[match.end():end]
+                string = string[end+1:]
+                yield token
+            elif match:
+                string = string[match.end():]
+                yield match.group(1)
+            elif string.strip():
+                raise xmlrpc.Fault(1, "Unexpected token: %s" % string)
+            else:
+                break
 
     def xmlrpc_delServiceDowntime(self, key):
         """Cancel all service downtimes identified by key"""
@@ -457,6 +406,3 @@ class NagiosXMLRPC(xmlrpc.XMLRPC):
                 count += 1
 
         return count
-
-
-
