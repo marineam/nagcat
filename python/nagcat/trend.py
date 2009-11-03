11e73550c10af4f6757c98078d037a8422c5af56
Michael Marineau <mmarineau@itasoftware.com>
2009-11-03 04:43:30 +0000

    Move the new Graph class into trend.py
    Now that the graph generator is more generic we can move it to trend.py
    so it is easily accessible via the nagcat package.
    
    This makes no changes to the moved code.

--- python/nagcat/trend.py
+++ python/nagcat/trend.py
@@ -18,6 +18,7 @@ import os
 import re
 import time
 import ctypes
+import tempfile
 
 from twisted.internet import reactor
 
@@ -393,3 +394,181 @@ class Trend(object):
                 log.error("rrd update to %s failed: %s" % (self._rrafile, ex))
 
         reactor.callInThread(do_update)
+
+class Colorator(object):
+    """A helper for picking graph colors"""
+
+    COLORS = ('#002A8F','#DA4725','#008A6D','#00BD27','#CCBB00','#F24AC8')
+
+    def __init__(self):
+        self.color_idx = 0
+
+    def next(self):
+        self.color_idx += 1
+        return Colorator.COLORS[(self.color_idx - 1) % len(Colorator.COLORS)]
+
+def rrd_esc(string):
+    """Escape : so rrd arguments parse properly"""
+    return string.replace(':', r'\:')
+
+class Graph(object):
+
+    def __init__(self, dir, host, rrd, period="day"):
+        self.rrd = rrd
+        self.host = host
+        path = "%s/%s/%s" % (dir, host, rrd)
+        self.rrd_path = "%s.rrd" % path
+        self.info = rrdtool_info(self.rrd_path)
+        self.conf = coil.parse_file("%s.coil" % path)
+        self.color = Colorator()
+        self.period = period
+
+        if period not in ('day', 'week', 'month', 'year'):
+            raise ValueError("Invalid period parameter")
+
+        self.args = []
+        self.ds = []
+        self._init_args()
+        self._init_ds()
+        self._init_ds_args()
+
+    def _init_ds(self):
+        """Find the data sources referred to in the coil config."""
+
+        self.ds.append('_state')
+
+        if self.conf.get('trend.type', False):
+            self.ds.append('_result')
+
+        if self.conf['query.type'] == 'compound':
+            for name, sub in self.conf['query'].iteritems():
+                if (isinstance(sub, coil.struct.Struct) and
+                        sub.get('trend.type', False)):
+                    self.ds.append(name)
+
+        elif self.conf.get('query.trend.type', False):
+            self.ds.append('query')
+
+    def _init_args(self):
+        """Build the initial part of the rrdgraph args
+        before the data source arguments
+        """
+
+        title = self.conf.get('trend.title',
+                "%s - %s" % (self.host, self.rrd))
+        axis_min = self.conf.get('trend.axis_min', "0")
+        axis_max = self.conf.get('trend.axis_max', None)
+        axis_label = self.conf.get('trend.axis_label', None)
+        base = self.conf.get('trend.base', 1000)
+
+        self.args = ["-s", "-1%s" % self.period,
+                     "--title", title,
+                     "--alt-autoscale-max", "--alt-y-grid",
+                     "--lower-limit", str(axis_min)]
+        if axis_max:
+            self.args += ["--upper-limit", str(axis_max)]
+        if axis_label:
+            self.args += ["--vertical-label", str(axis_label)]
+        if base:
+            self.args += ["--base", str(base)]
+
+        # Add the _state ds that all of them have
+        self.args += ["DEF:_state=%s:_state:MAX" % rrd_esc(self.rrd_path),
+                      "CDEF:_state_ok=_state,0,EQ",
+                      "CDEF:_state_warn=_state,1,EQ",
+                      "CDEF:_state_crit=_state,2,EQ",
+                      "CDEF:_state_unkn=_state,3,EQ",
+                      "TICK:_state_ok#ddffcc:1.0:Ok",
+                      "TICK:_state_warn#ffffcc:1.0:Warning",
+                      "TICK:_state_crit#ffcccc:1.0:Critical",
+                      "TICK:_state_unkn#ffcc55:1.0:Unknown\\n"]
+
+    def _init_ds_args(self):
+        """Build the rrdgraph args for all the known data sources"""
+
+        extra = set(self.ds)
+        for ds in self.ds:
+            if ds not in self.info['ds']:
+                self.args.append("COMMENT:WARNING\: Missing DS %s\\n" % ds)
+                continue
+
+            extra.remove(ds)
+
+            if ds == '_state':
+                continue
+
+            self.args += self._one_ds_args(ds)
+
+        for ds in extra:
+            self.args.append("COMMENT:WARNING\: Unexpected DS %s\\n" % ds)
+
+    def _one_ds_args(self, ds):
+        """Build the arguments for a single data source"""
+
+        args = []
+
+        if ds == "_result":
+            dsconf = self.conf
+            label = dsconf.get('trend.label', dsconf.get('label', 'Result'))
+            default_color = "#000000"
+        elif ds == 'query' and self.conf['query.type'] != 'compound':
+            dsconf = self.conf
+            label = dsconf.get('query.trend.label',
+                    dsconf.get('query.label', dsconf.get('label', 'Result')))
+            default_color = "#000000"
+        else:
+            dsconf = self.conf['query'][ds]
+            label = dsconf.get('trend.label',
+                    dsconf.get('label', ds.capitalize()))
+            default_color = self.color.next()
+
+        scale = int(dsconf.get('trend.scale', 0))
+        if scale:
+            args.append("DEF:_raw_%s=%s:%s:AVERAGE" %
+                    (ds, rrd_esc(self.rrd_path), ds))
+            args.append("CDEF:%s=_raw_%s,%d,*" % (ds, ds, scale))
+        else:
+            args.append("DEF:%s=%s:%s:AVERAGE" %
+                    (ds, rrd_esc(self.rrd_path), ds))
+
+        if dsconf.get('trend.stack', False):
+            stack = "STACK"
+        else:
+            stack = ""
+
+        color = dsconf.get('trend.color', default_color)
+        display = dsconf.get('trend.display', 'line').lower()
+        if display == 'area':
+            args.append("AREA:%s%s:%s:%s" % (ds, color, rrd_esc(label), stack))
+        elif display == 'line':
+            args.append("LINE2:%s%s:%s:%s" % (ds, color, rrd_esc(label), stack))
+        else:
+            raise ValueError("Invalid display value")
+
+        prefix = max(7 - len(label), 0) * " "
+        args.append("VDEF:_last_%s=%s,LAST" % (ds, ds))
+        args.append("VDEF:_avg_%s=%s,AVERAGE" % (ds, ds))
+        args.append("VDEF:_max_%s=%s,MAXIMUM" % (ds, ds))
+        args.append("GPRINT:_last_%s:%sCurrent\\:%%8.2lf%%s" % (ds, prefix))
+        args.append("GPRINT:_avg_%s:Average\\:%%8.2lf%%s" % ds)
+        args.append("GPRINT:_max_%s:Maximum\\:%%8.2lf%%s\\n" % ds)
+
+        return args
+
+    def graph(self, width=500, height=120):
+        fd, path = tempfile.mkstemp('.png')
+        try:
+            rrdtool.graph(path, "-a", "PNG", "--width", str(width),
+                    "--height", str(height), *self.args)
+        except:
+            os.close(fd)
+            raise
+        finally:
+            os.unlink(path)
+
+        fd = os.fdopen(fd)
+        png = fd.read()
+        fd.close()
+
+        return png
+
