21b3a85ff250588cbaf95c91a46176b738a2dedb
Michael Marineau <mmarineau@itasoftware.com>
2010-04-30 19:00:06 +0000

    Add a new table filter for parsing CSV and similar formats

---
+++ python/nagcat/plugins/filter_table.py
@@ -0,0 +1,115 @@
+# Copyright 2008-2010 ITA Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Table (csv, etc) Filter"""
+
+import csv
+from cStringIO import StringIO
+from zope.interface import classProvides
+from nagcat import errors, filters, log
+
+class RegexFilter(filters._Filter):
+    """Filter data based on a regular expression"""
+
+    classProvides(filters.IFilter)
+
+    name = "table"
+
+    def __init__(self, test, default, arguments):
+        super(RegexFilter, self).__init__(test, default, arguments)
+
+        args = self.arguments.split(',', 1)
+        self.row = args[0]
+        if args[0] and self.row.isdigit():
+            self.row = int(args[0])
+        elif args[0]:
+            raise errors.InitError(
+                    "Invalid row number in table filter: %r" % args[0])
+        else:
+            self.row = None
+
+        if len(args) == 2 and args[1]:
+            if args[1].isdigit():
+                self.col = int(args[1])
+            else:
+                self.col = args[1]
+        else:
+            self.col = None
+
+        if self.row is None and self.col is None:
+            raise errors.InitError("Empty table filter: %r" % self.arguments)
+
+    @errors.callback
+    def filter(self, result):
+        try:
+            return self._filter_without_default(result)
+        except errors.TestCritical:
+            if self.default is not None:
+                return self.default
+            else:
+                raise
+
+    def _filter_without_default(self, result):
+        log.debug("Fetching cell %s,%s from table", self.row, self.col)
+
+        try:
+            sniffer = csv.Sniffer()
+            dialect = sniffer.sniff(result)
+            reader = csv.reader(StringIO(result), dialect)
+            table = list(reader)
+        except csv.Error, ex:
+            raise errors.TestCritical("Failed to parse table: %s" % ex)
+
+        if not table:
+            raise errors.TestCritical("Empty table")
+
+        # If col is not an index, assume we have a header to work with.
+        if isinstance(self.col, str):
+            try:
+                col = table[0].index(self.col)
+            except ValueError:
+                raise errors.TestCritical("No such column %s" % self.col)
+        else:
+            col = self.col
+
+        if self.row is not None:
+            try:
+                row = table[self.row]
+            except IndexError:
+                raise errors.TestCritical("No such row %s, last row is %s" %
+                        (self.row, len(table)-1))
+
+            if col is not None:
+                try:
+                    return row[col]
+                except IndexError:
+                    raise errors.TestCritical("No such column %s in row %s" %
+                            (self.col, self.row))
+            else:
+                table = [row]
+        else:
+            data = []
+            for i, row in enumerate(table):
+                try:
+                    data.append([row[col]])
+                except IndexError:
+                    raise errors.TestCritical("No such column %s in row %s" %
+                            (self.col, i))
+            table = data
+
+        # Our result was a row or a column rather than cell so re-output
+        io = StringIO()
+        writer = csv.writer(io, dialect, lineterminator='\n')
+        writer.writerows(table)
+        return io.getvalue().rstrip()
---
+++ python/nagcat/unittests/filters/test_table.py
@@ -0,0 +1,67 @@
+# Copyright 2010 ITA Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from twisted.trial import unittest
+from nagcat import errors, filters
+
+class TableTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.csv = ("foo,bar,baz\n" "biz,buz,lol\n")
+        self.pw = ("root:x:0:0:root:/root:/bin/bash\n"
+                   "daemon:x:1:1:daemon:/usr/sbin:/bin/sh\n"
+                   "bin:x:2:2:bin:/bin:/bin/sh\n")
+        self.tab = ("Col1\tCol2\tCol3\n"
+                    "foo\tbar\tbaz\n"
+                    "biz\tbuz\tlol\n")
+
+    def testTrivial(self):
+        f = filters.Filter(object(), "table:0,0")
+        self.assertEquals(f.filter("foo,bar"), "foo")
+
+    def testCSV(self):
+        f = filters.Filter(object(), "table:1,1")
+        self.assertEquals(f.filter(self.csv), "buz")
+
+    def testPasswd(self):
+        f = filters.Filter(object(), "table:1,4")
+        self.assertEquals(f.filter(self.pw), "daemon")
+
+    def testTab(self):
+        f = filters.Filter(object(), "table:1,1")
+        self.assertEquals(f.filter(self.tab), "bar")
+
+    def testMissing(self):
+        f = filters.Filter(object(), "table:0,4")
+        self.assertIsInstance(f.filter("foo,bar"), errors.Failure)
+        f = filters.Filter(object(), "table:4,0")
+        self.assertIsInstance(f.filter("foo,bar"), errors.Failure)
+
+    def testDefault(self):
+        f = filters.Filter(object(), "table[x]:0,4")
+        self.assertEquals(f.filter("foo,bar"), "x")
+        f = filters.Filter(object(), "table[x]:4,0")
+        self.assertEquals(f.filter("foo,bar"), "x")
+
+    def testGetRow(self):
+        f = filters.Filter(object(), "table:1")
+        self.assertEquals(f.filter(self.csv), "biz,buz,lol")
+
+    def testGetCol(self):
+        f = filters.Filter(object(), "table:,1")
+        self.assertEquals(f.filter(self.csv), "bar\nbuz")
+
+    def getName(self):
+        f = filters.Filter(object(), "table:1,Col2")
+        self.assertEquals(f.filter(self.tab), "bar")
