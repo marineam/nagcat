3a8f783c0294d4a0cf78de0c5ed4a4dc4a2c4cd9
Michael Marineau <mmarineau@itasoftware.com>
2010-05-13 21:40:37 +0000

    Rebase OraclePLSQL onto OracleBase and code cleanup.
    The two oracle tests now share a common base and now both support
    timeouts! Yay! This also does a big cleanup of the plsql test which
    reduces its size considerably.

--- python/nagcat/plugins/query_oracle.py
+++ python/nagcat/plugins/query_oracle.py
@@ -32,7 +32,7 @@ try:
 except ImportError:
     cx_Oracle = None
 
-from nagcat import errors, log, query
+from nagcat import errors, query
 
 
 class _DBColumn:
@@ -57,13 +57,19 @@ class _DBColumn:
 
     def element(self, value):
         """Factory for creating an Element for this column"""
-        name = self.CLEAN_NAME_RE.sub("", self.name.lower())
+        return self.single_element(value, self.name, self.type)
+
+    @classmethod
+    def single_element(cls, value, name, data_type):
+        """Factory for creating a single Element"""
+
+        name = cls.CLEAN_NAME_RE.sub("", name.lower())
 
         # XML tags may not start with a digit
         if name[0].isdigit():
             name = "_%s" % name
 
-        elt = etree.Element(name, type=self.type.__name__)
+        elt = etree.Element(name, type=data_type.__name__)
         if value is not None:
             elt.text = str(value)
         return elt
@@ -241,205 +247,89 @@ class OracleSQL2(OracleSQL):
         self.name = "oracle_sql"
 
 
-class OraclePLSQL(query.Query):
-    """A query that uses cx_oracle directly (allowing for stored procedure calls)
-    results (via "out" parameters) are returned in XML
-    """
+class OraclePLSQL(OracleBase):
+    """Execute a stored procedure in Oracle, the result is formatted as XML"""
 
     classProvides(query.IQuery)
 
     name = "oracle_plsql"
 
-    # fields we expect to see in the conf
-    CONF_FIELDS = ['user', 'password', 'dsn', 'procedure', 'parameters', 'DBI']
-
-    # these are the orderings for the parameters in the config. I would have
-    # preferred to specify the parameters as dicts, but coil apparently does not
-    # support that yet.
-    IN_PARAMETER_FIELDS = ['direction', 'name', 'value']
-    OUT_PARAMETER_FIELDS = ['direction', 'name', 'type']
-
     def __init__(self, conf):
-        if not etree or not cx_Oracle:
-            raise errors.InitError(
-                "cx_Oracle and lxml are required for Oracle support.")
         super(OraclePLSQL, self).__init__(conf)
-        for fieldname in self.CONF_FIELDS:
-            if fieldname in conf:
-                self.conf[fieldname] = conf.get(fieldname)
 
-        self.check_config(conf)
+        self.conf['procedure'] = conf.get('procedure', None)
+        if not self.conf['procedure']:
+            raise errors.ConfigError(conf, "procedure is required")
 
-        # setup the DBI, if we don't have it
-        if "DBI" not in self.conf:
-            self.conf['DBI'] = "%s/%s@%s" % (
-                self.conf['user'], self.conf['password'], self.conf['dsn'])
-
-        # data members to be filled in later
-        self.connection = None
-        self.parameters = None
-        self.cursor = None
-
-
-    def check_config(self, conf):
-        """check the config for semantic errors"""
-
-        if not ('user' in self.conf and 'password' in self.conf
-                and 'dsn' in self.conf) and not 'DBI' in self.conf:
+        parameters = conf.get('parameters', None)
+        self.conf['parameters'] = []
+        if not parameters or not isinstance(parameters, list):
             raise errors.ConfigError(conf,
-                "needs values for user, password, dsn or for DBI")
+                    "parameters must be a list of lists")
 
-        if 'procedure' not in self.conf:
-            raise errors.ConfigError(conf, "needs a 'procedure' name to call")
+        for param in parameters:
+            self.conf['parameters'].append(
+                    self._check_params(conf, param))
 
-        # check the format of the parameters list.
+    def _check_params(self, conf, param):
+        """Check that the parameter definition is valid"""
+
+        if not isinstance(param, list):
+            raise errors.ConfigError(conf,
+                    "parameters must be a list of lists")
+
+        if len(param) != 3:
+            raise errors.ConfigError(conf,
+                    ("%s must be a list of three elements: "
+                     "[ <in|out> <param_name> <type|value>" % param))
+
+        param[0] = param[0].lower()
+        if param[0] not in ('in', 'out'):
+            raise errors.ConfigError(conf,
+                    "Invalid direction %s, must be 'in' or 'out'" % param[0])
+
+        if param[0] == "out":
+            type_name = param[2].upper()
+            type_class = getattr(cx_Oracle, type_name, None)
+            if not type_class:
+                raise errors.ConfigError(conf,
+                        "%s is not a valid Oracle type" % param[2])
+            param[2] = type_class
+
+        return param
+
+    def _build_params(self):
+        params = []
         for param in self.conf['parameters']:
-            if not isinstance(param, list):
-                raise errors.ConfigError(conf, '%s should be a list of lists'
-                                        % self.conf['parameters'])
+            if param[0] == "in":
+                params.append(param[2])
+            elif param[0] == "out":
+                params.append(self.cursor.var(param[2]))
+            else:
+                assert 0
+        return params
 
-            if len(param) != 3 or not param[0] in ['out', 'in']:
-                msg = ("%s should be a list of three elements: "
-                       "[ <in|out> <param_name> <type|value>" % param)
-                raise errors.ConfigError(conf, msg)
-
-
-    def buildparam(self, p):
-        """Parameters in the conf are in list form. Convert them to dicts (including
-        suitable DB variables where relevant) for easier sending/receiving"""
-
-        def makeDBtype(s):
-            "convert to the name of a cx_Oracle type, using the much-dreaded eval()"
-            try:
-                return eval('cx_Oracle.' + s.upper())
-            except AttributeError as err:
-                raise TypeError("'%s' is not a recognized Oracle type" % s)
-
-        if p[0].lower() == 'in':
-            retval = dict(zip(self.IN_PARAMETER_FIELDS, p))
-            retval['db_val'] = retval['value']
-            return retval
-        elif p[0].lower() == 'out':
-            retval = dict(zip(self.OUT_PARAMETER_FIELDS, p))
-            retval['db_val'] = self.cursor.var(makeDBtype(retval['type']))
-            return retval
-        else:
-            raise errors.InitError(
-                "Unrecognized direction '%s' in %s (expected 'in' or 'out')" % (p[0], p))
-
-
-    def _start(self):
-        log.debug("running procedure")
-
-        ## Should do some connection pooling here...
-        self.connection = cx_Oracle.Connection(self.conf['DBI'], threaded=True)
-        self.cursor = self.connection.cursor()
-
-        self.parameters = [self.buildparam(p) for p in self.conf['parameters']]
-        # result is a modified copy of self.parameters
-
-        #result = self.callproc(self.conf['query.procedure'], self.parameters)
-        db_params = [p['db_val']  for p in self.parameters]
-        self.deferred = threads.deferToThread(self.cursor.callproc,
-                                              self.conf['procedure'],
-                                              db_params)
-        self.deferred.addCallback(self._success)
-        self.deferred.addErrback(self._failure_oracle)
-        return self.deferred
+    def _start_oracle(self):
+        deferred = threads.deferToThread(self.cursor.callproc,
+                self.conf['procedure'], self._build_params())
+        deferred.addCallback(self._success)
+        return deferred
 
     @errors.callback
-    def _failure_oracle(self, result):
-        log.debug("Fail! %s", result.value)
-        self._cleanup()
-        raise_oracle_warning(result)
-
-    def _cleanup(self):
-        """Closes the DB connection"""
-        self.connection.close()
-
     def _success(self, result):
-        """Callback for the deferred that handles the procedure call"""
-        self.result = self._outparams_as_xml(result)
-        self._cleanup()
-        return self.result
-
-    def _outparams_as_xml(self, result_set):
         """Convert the 'out' parameters into XML. """
 
-        def only_out_params(resultset):
-            """(too big for a lambda) only convert those parameters that were
-            direction='out', along with their matching definitions from the conf"""
-            return [p for p in zip(self.parameters, result_set)
-                    if p[0]['direction'] == 'out']
-        try:
-            root = etree.Element('result')
-            for param, db_value in only_out_params(result_set):
-                if not isinstance(db_value, cx_Oracle.Cursor):
-                    # for non-cursor results, it is just one value
-                    tree = etree.Element(param['name'],
-                            type=param['db_val'].__class__.__name__)
-                    if db_value is not None: tree.text = str(db_value)
-                else:
-                    # for cursors, we will convert to tables
-                    columns = map(_DBColumn, db_value.description)
-                    table = db_value.fetchall()
-                    tree = _result_as_xml(columns, table, param['name'])
+        root = etree.Element('result')
+        for i, param in enumerate(self.conf['parameters']):
+            if param[0] != 'out':
+                continue
+
+            if isinstance(result[i], cx_Oracle.Cursor):
+                tree = self._to_xml(result[i], param[1])
                 root.append(tree)
-            return etree.tostring(root, pretty_print=True)
+            else:
+                item = _DBColumn.single_element(
+                        result[i], param[1], param[2])
+                root.append(item)
 
-        except Exception as err:
-            raise errors.TestCritical("XML conversion error!: %s" % err)
-
-
-def _result_as_xml(columns, result_table, name="queryresult"):
-        """Convert an executed query into XML, using the columns to get the
-        names and types of the column tags"""
-
-        # example query: select 1 as foo from dual
-        # returns: '<queryresult><row><foo type="NUMBER">1</foo></row></queryresult>'
-        try:
-            tree = etree.Element(name)
-            for row in result_table:
-                xmlrow = etree.Element('row')
-                for col, val in zip(columns, row):
-                    xmlrow.append(_xml_element(col, val))
-                tree.append(xmlrow)
-            return tree
-        except Exception as err:
-            raise errors.TestCritical("XML conversion error!: %s" % err)
-
-
-def _xml_element(col, value):
-        name = re.sub("[^\w]","", col.name.lower())
-        elt = etree.Element(name, type=col.type.__name__)
-        if value != None:
-            elt.text = str(value)
-        return elt
-
-
-def raise_oracle_warning(failure):
-    """A handy wrapper for handling cx_Oracle failures in Query objects"""
-
-    if isinstance(failure.value, cx_Oracle.Warning):
-        # Exception raised for important warnings and defined by the DB API
-        # but not actually used by cx_Oracle.
-        raise errors.TestWarning(failure.value)
-
-    if isinstance(failure.value, cx_Oracle.InterfaceError):
-        # Exception raised for errors that are related to the database
-        # interface rather than the database itself. It is a subclass of
-        # Error.
-        raise errors.TestCritical(failure.value)
-
-    if isinstance(failure.value, cx_Oracle.DatabaseError):
-        # Exception raised for errors that are related to the database. It
-        # is a subclass of Error.
-        raise errors.TestCritical(failure.value)
-
-    if isinstance(failure.value, cx_Oracle.Error):
-        # Exception that is the base class of all other exceptions
-        # defined by cx_Oracle and is a subclass of the Python
-        # StandardError exception (defined in the module exceptions).
-        raise errors.TestCritical(failure.value)
-
-    log.debug("Unhandled failure! %s", failure)
+        return etree.tostring(root, pretty_print=True)
