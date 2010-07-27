89f28596b56f8895983284ec795d1727e4e64985
Jonathan Yu <jyu@itasoftware.com>
2010-07-27 22:51:10 +0000

    Major code refactoring to reduce calls to ObjectParser

--- railroad/railroad/viewhosts/views.py
+++ railroad/railroad/viewhosts/views.py
@@ -17,65 +17,120 @@ import rrdtool, os, coil, sys, time
 from django.conf import settings
 from django.http import HttpResponse
 from django.template import Context, loader
-from railroad.parserrd.views import graphable, is_graphable
 from nagcat import nagios_objects
 from railroad.errors import RailroadError
 
-data_path = settings.DATA_PATH
-stat_file = data_path + 'status.dat'
-obj_file = data_path + 'objects.cache'
+def is_graphable(host, service):
+    rra_path = settings.RRA_PATH
+    coilfile = rra_path + host + '/' + service + '.coil'
+    rrd = rra_path + host + '/' + service + '.rrd'
+    if(os.path.exists(coilfile) and os.path.exists(rrd)):
+        coilstring = open(coilfile).read()
+        coilstruct = coil.parse(coilstring)
+        query = coilstruct.get('query')
 
-def hostlist():
-    try:
-        return nagios_objects.ObjectParser(stat_file, ('host',))['host']
-    except IndexError as e:
-        raise RailroadError('Unexpected ObjectParser failure')
+        # rrdtool hates unicode strings, and Django gives us one, 
+        # so convert to ascii
+        rrdslice = rrdtool.fetch(str(rrd),
+                    '--start', '0',
+                    '--end', '10',
+                    '--resolution', '1',
+                    'AVERAGE')
+
+        try:
+            rrdslice[1].index('_state')
+            return True
+        except ValueError:
+            for key in query.keys():
+                val = query.get(key)
+                if type() == type(query) and val.has_key('trend'):
+                    return True
+            return False
+    return False
 
 
-def hostlist_by_group(group):
-    group_list = nagios_objects.ObjectParser(obj_file, ('hostgroup'), {'alias': group})['hostgroup']
-    all_hosts = hostlist()
-    host_list = []
-    try:
-        group_dict = group_list[0]
-        target = group_dict['members'].split(',')
-        for host in all_hosts:
-            if host['host_name'] in target:
-                host_list.append(host)
-        return host_list
-    except IndexError as e:
-        raise RailroadError(group + ' not found in objects.cache')
+def are_graphable(host, service_list):
+    rra_path = settings.RRA_PATH
+    for service in service_list:
+        coilfile = rra_path + host + '/'    \
+            + service['service_description'] + '.coil'
+        rrd = rra_path + host + '/'         \
+            + service['service_description'] + '.rrd'
+        if(os.path.exists(coilfile) and os.path.exists(rrd)):
+            coilstring = open(coilfile).read()
+            coilstruct = coil.parse(coilstring)
+            query = coilstruct.get('query')
 
-def hostdetail(host):
-    try:
-        host_detail = nagios_objects.ObjectParser(stat_file, \
-                        ('host',), {'host_name': host})['host']
-        return host_detail[0]
-    except IndexError as e:
-        raise RailroadError(group + ' not found in objects.cache')
+            # rrdtool hates unicode strings, and Django gives us one, 
+            # so convert to ascii
+            rrdslice = rrdtool.fetch(str(rrd),
+                        '--start', '0',
+                        '--end', '10',
+                        '--resolution', '1',
+                        'AVERAGE')
 
-def grouplist():
-    group_list = nagios_objects.ObjectParser(obj_file,  \
-                    ('hostgroup',))['hostgroup']
-    return group_list
+            try:
+                rrdslice[1].index('_state')
+                service['is_graphable'] = True
+            except ValueError:
+                for key in query.keys():
+                    val = query.get(key)
+                    if type() == type(query) and val.has_key('trend'):
+                        service['is_graphable'] = True
+                        break
+                service['is_graphable'] = False
 
-def servicelist(host):
-    status = nagios_objects.ObjectParser(stat_file,     \
-                    ('service'), {'host_name': host})
-    services = ""
-    service_list = status['service']
-    service_list = zip(service_list, graphable(host, service_list))
-    return service_list
+        else:
+            service['is_graphable'] = False
 
-def servicedetail(host, service):
-    service_list = nagios_objects.ObjectParser(stat_file, ('service'),  \
-                {'host_name': host, 'service_description': service})
-    try:
-        return service_list['service'][0]
-    except KeyError as e:
-        return None
-    except IndexError as e:
-        return None
+def parse():
+    data_path = settings.DATA_PATH
+    stat_file = data_path + 'status.dat'
+    obj_file = data_path + 'objects.cache'
+    stat = nagios_objects.ObjectParser(stat_file, ('service','host'),)
+    obj = nagios_objects.ObjectParser(obj_file, ('hostgroup'), )
+    return (stat,obj)
+
+def grouplist(obj):
+    return obj['hostgroup']
+
+def hostlist(stat):
+    return stat['host']
+
+def servicelist(stat):
+    return stat['service']
+
+def groupdetail(obj, group_name):
+    group_list = grouplist(obj)
+    for group in group_list:
+        if group['alias'] == group_name:
+            return group
+
+def hostdetail(stat, host_name):
+    host_list = hostlist(stat)
+    for host in host_list:
+        if host['host_name'] == host_name:
+            return host
+
+def servicelist_by_description(stat, service_description):
+    all_services = servicelist(stat)
+    return [service for service in all_services if service['service_description'] == service_description]
+
+def servicedetail(stat, host, service_name):
+    all_services = servicelist(stat)
+    for service in all_services:
+        if service['host_name'] == host and service['service_description'] == service_name:
+            return service
+
+def hostlist_by_group(stat, obj, group_name):
+    group = groupdetail(obj, group_name)
+    all_hosts = hostlist(stat)
+    target = group['members'].split(',')
+    return [host for host in all_hosts if host['host_name'] in target]
+
+def servicelist_by_host(stat, host):
+    all_services = servicelist(stat)
+    return [service for service in all_services if service['host_name'] == host]
 
 
 def get_time_intervals():
@@ -86,9 +141,9 @@ def get_time_intervals():
 
 # TODO: This should probably just be a class that wraps Context, as we
 # will have to include this on each and every page
-def add_hostlist(c):
-    groups = grouplist()
-    hosts = hostlist()
+def add_hostlist(stat, obj, c):
+    groups = grouplist(obj)
+    hosts = hostlist(stat)
     sidebar = []
 
     for group in groups:
@@ -111,15 +166,17 @@ def add_hostlist(c):
 
 def index(request):
     t = loader.get_template('index.html')
+    stat,obj = parse()
     context_data = {}
-    context_data = add_hostlist(context_data)
+    context_data = add_hostlist(stat, obj, context_data)
     c = Context(context_data)
     return HttpResponse(t.render(c))
 
 def host(request, host):
     t = loader.get_template('host.html')
-    services = servicelist(host)
-    host_detail = hostdetail(host)
+    stat,obj = parse()
+    services = servicelist_by_host(stat, host)
+    host_detail = hostdetail(stat, host)
     ending = int(time.time())
     starting = ending - 86400
     context_data = {
@@ -129,13 +186,14 @@ def host(request, host):
         'time_interval': [starting,ending]
     }
     
-    context_data = add_hostlist(context_data)
+    context_data = add_hostlist(stat, obj, context_data)
     c = Context(context_data)
     return HttpResponse(t.render(c))
 
 def service(request, host, service):
     t = loader.get_template('service.html')
-    service_detail = servicedetail(host, service)
+    stat,obj = parse()
+    service_detail = servicedetail(stat, host, service)
 
     str = service_detail.get('plugin_output','')
     if str:
@@ -152,33 +210,26 @@ def service(request, host, service):
         'time_intervals': time_intervals
     }
 
-    context_data = add_hostlist(context_data)
+    context_data = add_hostlist(stat, obj, context_data)
     c = Context(context_data)
     return HttpResponse(t.render(c))
 
 def group(request, group):
     t = loader.get_template('group.html')
+    stat,obj = parse()
     service_dict = {}
         
-    host_list = hostlist_by_group(group)
+    host_list = hostlist_by_group(stat, obj, group)
     host_names = map(lambda x: x['host_name'], host_list)
 
-    def test():
-        test1 = nagios_objects.ObjectParser(stat_file, ('service','host'), )
-        test2 = nagios_objects.ObjectParser(obj_file, ('host_group'), )
-
-    test()
-
-    service_list = nagios_objects.ObjectParser(stat_file, ('service'), {'host_name': host_names})['service']
+    service_list = servicelist(stat)
 
     for service in service_list:
         service_name = service['service_description']
-        if service_name in service_dict:
-            continue
+        if service['host_name'] in host_names:
+            service_dict[service_name] = True
 
-        service_dict[service_name] = service['host_name'] in host_names
-
-    services = filter(lambda x: service_dict[x], service_dict.keys())
+    services = service_dict.keys()
     host_list.sort(lambda x,y: cmp(x['host_name'],y['host_name']))
     services.sort()
 
@@ -201,15 +252,15 @@ def group(request, group):
         'time_interval': [starting,ending]
     }
     
-    context_data = add_hostlist(context_data)
+    context_data = add_hostlist(stat, obj, context_data)
     c = Context(context_data)
     return HttpResponse(t.render(c))
 
 def groupservice(request, group, service):
     t = loader.get_template('groupservice.html')
-    host_list = hostlist_by_group(group)
-    service_list = nagios_objects.ObjectParser(stat_file, ('service'),  \
-                        {'service_description': service})['service']
+    stat,obj = parse()
+    host_list = hostlist_by_group(stat, obj, group)
+    service_list = servicelist_by_description(stat, service)
 
     def has_service(host):
         for service in service_list:
@@ -239,6 +290,6 @@ def groupservice(request, group, service):
         'true': True
     }
     
-    context_data = add_hostlist(context_data)
+    context_data = add_hostlist(stat, obj, context_data)
     c = Context(context_data)
     return HttpResponse(t.render(c))
