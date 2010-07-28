4e651bcff59dca9261de9b3faed74782a5e878f4
Eric Harmon <eharmon@itasoftware.com>
2010-07-28 15:53:47 +0000

    Added cachegrind output support to profiling

--- railroad/railroad/middleware/profiler.py
+++ railroad/railroad/middleware/profiler.py
@@ -12,7 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Based on this snippet: http://djangosnippets.org/snippets/1579/
+# Middleware based on this snippet: http://djangosnippets.org/snippets/1579/
+
+# CacheGrind output based on lsprofcalltree.py by:
+# David Allouche
+# Jp Calderone
+# Itamar Shtull-Trauring
+# Johan Dahlin
 
 import cProfile
 import os
@@ -21,6 +27,81 @@ import tempfile
 from cStringIO import StringIO
 
 from django.conf import settings
+
+def label(code):
+    if isinstance(code, str):
+        return ('~', 0, code)    # built-in functions ('~' sorts at the end)
+    else:
+        return '%s %s:%d' % (code.co_name,
+                             code.co_filename,
+                             code.co_firstlineno)
+
+class KCacheGrind(object):
+    def __init__(self, profiler):
+        self.data = profiler.getstats()
+        self.out_file = None
+
+    def output(self, out_file):
+        self.out_file = out_file
+        print >> out_file, 'events: Ticks'
+        self._print_summary()
+        for entry in self.data:
+            self._entry(entry)
+
+    def _print_summary(self):
+        max_cost = 0
+        for entry in self.data:
+            totaltime = int(entry.totaltime * 1000)
+            max_cost = max(max_cost, totaltime)
+        print >> self.out_file, 'summary: %d' % (max_cost,)
+
+    def _entry(self, entry):
+        out_file = self.out_file
+
+        code = entry.code
+        #print >> out_file, 'ob=%s' % (code.co_filename,)
+        if isinstance(code, str):
+            print >> out_file, 'fi=~'
+        else:
+            print >> out_file, 'fi=%s' % (code.co_filename,)
+        print >> out_file, 'fn=%s' % (label(code),)
+
+        inlinetime = int(entry.inlinetime * 1000)
+        if isinstance(code, str):
+            print >> out_file, '0 ', inlinetime
+        else:
+            print >> out_file, '%d %d' % (code.co_firstlineno, inlinetime)
+
+        # recursive calls are counted in entry.calls
+        if entry.calls:
+            calls = entry.calls
+        else:
+            calls = []
+
+        if isinstance(code, str):
+            lineno = 0
+        else:
+            lineno = code.co_firstlineno
+
+        for subentry in calls:
+            self._subentry(lineno, subentry)
+        print >> out_file
+
+    def _subentry(self, lineno, subentry):
+        out_file = self.out_file
+        code = subentry.code
+        #print >> out_file, 'cob=%s' % (code.co_filename,)
+        print >> out_file, 'cfn=%s' % (label(code),)
+        if isinstance(code, str):
+            print >> out_file, 'cfi=~'
+            print >> out_file, 'calls=%d 0' % (subentry.callcount,)
+        else:
+            print >> out_file, 'cfi=%s' % (code.co_filename,)
+            print >> out_file, 'calls=%d %d' % (
+                subentry.callcount, code.co_firstlineno)
+
+        totaltime = int(subentry.totaltime * 1000)
+        print >> out_file, '%d %d' % (lineno, totaltime)
 
 class ProfilerMiddleware(object):
     def process_view(self, request, callback, callback_args, callback_kwargs):
@@ -32,16 +113,24 @@ class ProfilerMiddleware(object):
     def process_response(self, request, response):
         if settings.DEBUG and 'prof' in request.GET:
             (fd, self.profiler_file) = tempfile.mkstemp()
-            self.profiler.dump_stats(self.profiler_file)
-#            out = StringIO()
-#            stats = pstats.Stats(self.profiler_file, stream=out)
-#            stats.strip_dirs()          # Must happen prior to sort_stats
-#            if request.GET['prof']:
-#                stats.sort_stats(request.GET['prof'])
-#            stats.print_stats()
-#            os.unlink(self.profiler_file)
-#            response.content = '<pre>%s</pre>' % out.getvalue()
-            f = open(self.profiler_file)
-            response.content = f.read()
+            if 'snake' in request.GET:
+                self.profiler.dump_stats(self.profiler_file)
+    #            out = StringIO()
+    #            stats = pstats.Stats(self.profiler_file, stream=out)
+    #            stats.strip_dirs()          # Must happen prior to sort_stats
+    #            if request.GET['prof']:
+    #                stats.sort_stats(request.GET['prof'])
+    #            stats.print_stats()
+    #            os.unlink(self.profiler_file)
+    #            response.content = '<pre>%s</pre>' % out.getvalue()
+                f = open(self.profiler_file)
+                response.content = f.read()
+            elif 'cg' in request.GET:
+                kg = KCacheGrind(self.profiler)
+                f = open(self.profiler_file, 'r+')
+                kg.output(f)
+                f.seek(0)
+                response.content = f.read()
+            response['Content-Disposition'] = 'attachment; filename=profile'
             response['Content-Type'] = 'application/octet-stream'
         return response
