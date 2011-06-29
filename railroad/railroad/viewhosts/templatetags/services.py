498a12b8eb5f8751507982edfedf2f97a24ad21a
Mike Cooper <mythmon@gmail.com>
2011-06-29 16:47:37 -0400

    Add a nagios number -> state django filter.

---
+++ railroad/railroad/viewhosts/templatetags/services.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+state_names = {0: 'OK', 1: 'Warning', 2: 'Critical', 3: 'Unknown'}
+
+@register.filter
+def state_name(value):
+    """Turn a nagio state number into a state name."""
+    return state_names[int(value)]
